64f1cbfa0450899c903ddf2966ac4458905c1cf8
yuko <dev1@yukoleather.net>
2021-01-30 16:01:21 +0600

    Initial commit

---
+++ blog_website/blog_app/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.user_register, name='register'),
+    path('login', views.user_login, name='login'),
+    path('logout', views.user_logout, name='logout'),
+    path('post_blog', views.post_blog, name='post_blog'),
+    path('blog_details/<int:id>', views.blog_details, name='blog_details'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
+
+]
